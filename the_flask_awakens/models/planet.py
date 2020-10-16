403338b653cc1a029399ac458b65e151111b889a
Davide Bucher <>
2020-10-16 15:08:52 +0200

    no message

--- the_flask_awakens/models/planet.py
+++ the_flask_awakens/models/planet.py
@@ -2,9 +2,26 @@ from flask import (
     Blueprint, request, url_for, current_app, jsonify
 )
 
+from bson import json_util
+
+from ..auth import token_required
+from flask_pymongo import PyMongo
 import requests
+import re
 
 bp = Blueprint('planet', __name__, url_prefix='/planets')
+
+def cleanSwapiData(data, planet_id):
+
+    if planet_id is None:
+        # when we return the list we need to extract the id 
+        planet_id = re.findall('\d+', data['url'] )[0]
+
+    data['residents'] = None
+    data['films'] = None
+    data['planet_id'] = planet_id
+
+    return data
 
 @bp.route('/', methods=['GET'])
 def listPlanets():
@@ -18,7 +35,7 @@ def listPlanets():
         r = requests.get(endpoint)
         data = r.json()
         for planet in data['results']:
-            planets.append(planet)
+            planets.append(cleanSwapiData(planet, None))
 
         while data['next'] is not None:
             r = requests.get(data['next'])
@@ -29,3 +46,35 @@ def listPlanets():
         return jsonify({'error': 'something went wrong'})
     
     return jsonify({'planets': planets})
+
+@bp.route('/<planet_id>/', methods=['GET'])
+@token_required
+def getPlanet(payload, planet_id):
+
+    mongo = PyMongo(current_app)
+
+    # check if planet already exists in DB
+    planetsCollection = mongo.db.planets
+    planet = planetsCollection.find_one({'planet_id': planet_id})
+
+    # if no call swapi, add it to mongo, and return it
+    if planet is None:
+        try:
+            r = requests.get('http://swapi.dev/api/planets/'+planet_id+'/')
+            data = r.json()
+        except:
+            return jsonify({'error': 'something went wrong'})
+
+        clean_data = cleanSwapiData(data, planet_id)
+
+        new_planet_id = planetsCollection.insert(clean_data)
+        new_planet = planetsCollection.find_one({'planet_id': planet_id })
+
+        # Remove the objectID retrieved from mongo
+        new_planet.pop('_id', None)
+        return json_util.dumps(new_planet, default=json_util.default)
+    # if yes return it
+    else:
+        # Remove the objectID retrieved from mongo
+        planet.pop('_id', None)
+        return json_util.dumps(planet, default=json_util.default)
